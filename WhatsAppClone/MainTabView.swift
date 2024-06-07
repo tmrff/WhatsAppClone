1b609d29cda7159b5babd67abe3e1687641105f5
Thomas <Roff>
2024-06-07 13:17:20 +1200

    Add project

---
+++ WhatsAppClone/MainTabView.swift
@@ -0,0 +1,94 @@
+//
+//  MainTabView.swift
+//  WhatsAppClone
+//
+//  Created by Thomas on 7/06/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    init() {
+        makeTabBarOpaque()
+    }
+    
+    var body: some View {
+        TabView {
+            placeholderItemView("Updates")
+                .tabItem {
+                    Image(systemName: Tab.updates.icon)
+                    Text(Tab.updates.title)
+                }
+            placeholderItemView("Calls")
+                .tabItem {
+                    Image(systemName: Tab.calls.icon)
+                    Text(Tab.calls.title)
+                }
+            placeholderItemView("Communities")
+                .tabItem {
+                    Image(systemName: Tab.communities.icon)
+                    Text(Tab.communities.title)
+                }
+            placeholderItemView("Chat")
+                .tabItem {
+                    Image(systemName: Tab.chat.icon)
+                    Text(Tab.chat.title)
+                }
+            placeholderItemView("Settings")
+                .tabItem {
+                    Image(systemName: Tab.settings.icon)
+                    Text(Tab.settings.title)
+                }
+        }
+    }
+    
+    private func makeTabBarOpaque() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+}
+
+extension MainTabView {
+    private func placeholderItemView(_ title: String) -> some View {
+        ScrollView {
+            VStack {
+                ForEach(0..<120) { _ in
+                    Text(title)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 120)
+                        .background(Color.green.opacity(0.5))
+                }
+            }
+        }
+    }
+    private enum Tab: String {
+        case updates, calls, communities, chat, settings
+        
+        fileprivate var title: String {
+            return rawValue.capitalized
+        }
+        
+        fileprivate var icon: String {
+            switch self {
+            case .updates:
+                return "circle.dashed.inset.filled"
+            case .calls:
+                return "phone"
+            case .communities:
+                return "person.3"
+            case .chat:
+                return "message"
+            case .settings:
+                return "gear"
+            }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Thomas on 7/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
