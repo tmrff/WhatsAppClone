c6d7612b8b4112926557f7db47e96a4c3a74ee88
Thomas <Roff>
2024-06-19 00:55:20 +1200

    Deselect media items

--- WhatsAppClone/Screens/Chat/ViewModels/ChatRoomViewModel.swift
+++ WhatsAppClone/Screens/Chat/ViewModels/ChatRoomViewModel.swift
@@ -23,7 +23,7 @@ final class ChatRoomViewModel: ObservableObject {
     private var currentUser: UserItem?
     
     var showPhotoPickerPreview: Bool {
-        return !mediaAttachments.isEmpty
+        return !mediaAttachments.isEmpty || !photoPickerItems.isEmpty
     }
     
     init(_ channel: ChannelItem) {
@@ -105,16 +105,17 @@ final class ChatRoomViewModel: ObservableObject {
     private func parsePhotoPickerItems(_ photoPickerItems: [PhotosPickerItem]) async {
         for photoItem in photoPickerItems {
             if photoItem.isVideo {
-                if let movie = try? await photoItem.loadTransferable(type: VideoPickerTransferable.self), let thumbnailImage = try? await movie.url.generateVideoThumbnail() {
-                    let videoAttachment = MediaAttachment(id: UUID().uuidString, type: .video(thumbnailImage, movie.url))
+                if let movie = try? await photoItem.loadTransferable(type: VideoPickerTransferable.self), let thumbnailImage = try? await movie.url.generateVideoThumbnail(), let itemIdentifier = photoItem.itemIdentifier {
+                    let videoAttachment = MediaAttachment(id: itemIdentifier, type: .video(thumbnailImage, movie.url))
                     self.mediaAttachments.insert(videoAttachment, at: 0)
                 }
             } else {
                 guard
                     let data = try? await photoItem.loadTransferable(type: Data.self),
-                    let thumbnail = UIImage(data: data)
+                    let thumbnail = UIImage(data: data),
+                    let itemIdentifier = photoItem.itemIdentifier
                 else { return }
-                let photoAttachment = MediaAttachment(id: UUID().uuidString, type: .photo(thumbnail))
+                let photoAttachment = MediaAttachment(id: itemIdentifier, type: .photo(thumbnail))
                 self.mediaAttachments.insert(photoAttachment, at: 0)
             }
         }
@@ -136,6 +137,16 @@ final class ChatRoomViewModel: ObservableObject {
         case .play(let attachment):
             guard let fileURL = attachment.fileURL else { return }
             showMediaPlayer(fileURL)
+        case .remove(let attachment):
+            remove(attachment)
         }
+    }
+    
+    private func remove(_ item: MediaAttachment) {
+        guard let attachmentIndex = mediaAttachments.firstIndex(where: { $0.id == item.id }) else { return }
+        mediaAttachments.remove(at: attachmentIndex)
+        
+        guard let photoIndex = photoPickerItems.firstIndex(where: { $0.itemIdentifier == item.id }) else { return }
+        photoPickerItems.remove(at: photoIndex)
     }
 }
--- WhatsAppClone/Screens/Chat/Views/MediaAttachmentPreview.swift
+++ WhatsAppClone/Screens/Chat/Views/MediaAttachmentPreview.swift
@@ -37,7 +37,7 @@ struct MediaAttachmentPreview: View {
                 .cornerRadius(5)
                 .clipped()
                 .overlay(alignment: .topTrailing) {
-                    cancelButton()
+                    cancelButton(attachment)
                 }
                 .overlay(alignment: .center) {
                     playButton("play.fill", attachment: attachment)
@@ -46,9 +46,9 @@ struct MediaAttachmentPreview: View {
         }
     }
     
-    private func cancelButton() -> some View {
+    private func cancelButton(_ attachment: MediaAttachment) -> some View {
         Button {
-            
+            actionHandler(.remove(attachment))
         } label: {
             Image(systemName: "xmark")
                 .scaledToFit()
@@ -90,7 +90,7 @@ struct MediaAttachmentPreview: View {
         .cornerRadius(5)
         .clipped()
         .overlay(alignment: .topTrailing) {
-            cancelButton()
+            cancelButton(attachment)
         }
         .overlay(alignment: .bottomLeading) {
             Text("Test mp3 file name here")
@@ -112,6 +112,7 @@ extension MediaAttachmentPreview {
     
     enum UserAction {
         case play(_ item: MediaAttachment)
+        case remove(_ item: MediaAttachment)
     }
 }
 
