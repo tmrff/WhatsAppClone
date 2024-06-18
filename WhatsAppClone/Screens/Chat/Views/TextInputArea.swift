4867e58a4823db30ef309f5fa5dc75c1cc378e12
Thomas <Roff>
2024-06-19 01:50:13 +1200

    Add audio recorder view components

--- WhatsAppClone/Screens/Chat/Views/TextInputArea.swift
+++ WhatsAppClone/Screens/Chat/Views/TextInputArea.swift
@@ -10,17 +10,25 @@ import SwiftUI
 struct TextInputArea: View {
     @Binding var textMessage: String
     let actionHandler:(_ action: UserAction) -> Void
+    @State private var isRecording = false
+    @State private var isPulsing = false
     
     private var disableSendButton: Bool {
-        return textMessage.isEmptyOrWhiteSpace
+        return textMessage.isEmptyOrWhiteSpace || isRecording
     }
     
     var body: some View {
         HStack(alignment: .bottom, spacing: 5) {
             imagePickerButton()
                 .padding(3)
+            
             audioRecorderButton()
-            messageTextField()
+            if isRecording {
+                audioSessionIndicatorView()
+            } else {
+                messageTextField()
+            }
+            
             sendMessageButton()
                 .disabled(disableSendButton)
                 .grayscale(disableSendButton ? 0.8 : 0)
@@ -29,6 +37,35 @@ struct TextInputArea: View {
         .padding(.horizontal, 8)
         .padding(.top, 10)
         .background(.whatsAppWhite)
+        .animation(.spring, value: isRecording)
+    }
+    
+    private func audioSessionIndicatorView() -> some View {
+        HStack {
+            Image(systemName: "circle.fill")
+                .foregroundStyle(.red)
+                .font(.caption)
+                .scaleEffect(isPulsing ? 1.8 : 1.0)
+            
+            Text("Recording Audio")
+                .font(.callout)
+                .lineLimit(1)
+            
+            Spacer()
+            
+            Text("00:01")
+                .font(.callout)
+                .fontWeight(.semibold)
+        }
+        .padding(.horizontal, 8)
+        .frame(height: 30)
+        .frame(maxWidth: .infinity)
+        .clipShape(Capsule())
+        .background(
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(Color.blue.opacity(0.1))
+        )
+        .overlay(textViewBorder())
     }
     
     private func messageTextField() -> some View {
@@ -59,14 +96,18 @@ struct TextInputArea: View {
     
     private func audioRecorderButton() -> some View {
         Button {
-            
+            actionHandler(.recordAudio)
+            isRecording.toggle()
+            withAnimation(.easeInOut(duration: 1.5).repeatForever()) {
+                isPulsing.toggle()
+            }
         } label: {
-            Image(systemName: "mic.fill")
+            Image(systemName: isRecording ? "square.fill" : "mic.fill")
                 .fontWeight(.heavy)
                 .imageScale(.small)
                 .foregroundStyle(.white)
                 .padding(6)
-                .background(.blue)
+                .background(isRecording ? .red : .blue)
                 .clipShape(Circle())
                 .padding(.horizontal, 3)
         }
@@ -90,6 +131,7 @@ extension TextInputArea {
     enum UserAction {
         case presentPhotoPicker
         case sendMessage
+        case recordAudio
     }
 }
 
